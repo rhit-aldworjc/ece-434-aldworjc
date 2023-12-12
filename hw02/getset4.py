09d4675f6ba1409dab2c3176ffc8d6d87f8a8251
rhit-aldworjc <98357405+rhit-aldworjc@users.noreply.github.com>
2023-12-12 15:18:05 -0500

    added comments to getset4.py

--- hw02/getset4.py
+++ hw02/getset4.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
-# //////////////////////////////////////
-# 	getset.py
-#  Get the value of P8_16 and write it to P9_14. 
-#     P8_16 is line 14 on chip 1.  P9_14 is line 18 of chip 1.
-# 	Wiring:	Attach a switch to P8_16 and 3.3V and an LED to P9_14.
-# 	Setup:	sudo apt uupdate; sudo apt install libgpiod-dev
-#           Run: gpioinfo | grep -i -e chip -e P9_14 to find chip and line numbers
-# 	See:	https://github.com/starnight/libgpiod-example/blob/master/libgpiod-led/main.c
-# //////////////////////////////////////
-# Based on https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/tree/bindings/python/examples
+#Author: Jack Aldworth
+#Date Made: 12/12/2023
+#Desription: Changes LED states to match switch state
 
 import gpiod
 import sys
@@ -16,7 +9,7 @@ import sys
 CONSUMER='getset'
 CHIP='1'
 getoffsets=[14,15,12,13]# P8_16, P8_15, P8_12, P8_11
-setoffests=[18,19,28,29] # P9_14, P9_16, P8_5, P8_6
+setoffests=[18,19,28,29] # P9_14, P9_16, P9_12, P8_26
 
 chip = gpiod.Chip(CHIP)
 
